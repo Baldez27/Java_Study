53af6a1d589357ebed424082278fd698ccffeb51
Baldez27 <danielbaldez2000@gmail.com>
2023-04-16 21:20:03 +0044

    Teste

---
+++ ex_003.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ex_003 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        double valorMaior = 0;
+        double valorMenor = 0;
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Infome seu sexo (M/F): ");
+            String t = s.nextLine();
+
+        }
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Informe a sua altura: " );
+            double a = s.nextDouble();
+
+            if(a > valorMaior && a!=0) {
+                valorMaior = a;
+
+            }
+            if(a < valorMenor && a!=0) {
+                valorMenor = a;
+            }
+        }
+        System.out.println("Maior valor: "+ valorMaior);
+        System.out.println("Menor valor: "+ valorMenor);
+
+
+    }
+}
---
+++ out/production/Java_Study/README.md
@@ -0,0 +1,2 @@
+# Java_Study
+Estudos sobre Java.
