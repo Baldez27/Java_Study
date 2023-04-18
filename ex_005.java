aa35db3490c062c59f81833238cda3155d0aa437
Baldez27 <danielbaldez2000@gmail.com>
2023-04-18 11:42:46 +0044

    Execício

---
+++ ex_005.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ex_005 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int num = 15;
+
+        System.out.println(" Digite uma nome para o seu usuário: ");
+        String nome = s.nextLine();
+
+        System.out.println("Bem vindo " + nome + " seu usuário é o " + num + "° a se cadastrar em nosso site.");
+    }
+}
